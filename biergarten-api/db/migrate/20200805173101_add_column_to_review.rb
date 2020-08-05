4b423a558316ad6271d911b7369838cb352173a0
kevinneyer <kevinneyer@gmail.com>
2020-08-05 13:33:53 -0400

    updated review database

---
+++ biergarten-api/db/migrate/20200805173101_add_column_to_review.rb
@@ -0,0 +1,5 @@
+class AddColumnToReview < ActiveRecord::Migration[6.0]
+  def change
+    add_column :reviews, :rating, :float
+  end
+end
--- biergarten-api/db/schema.rb
+++ biergarten-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_153448) do
+ActiveRecord::Schema.define(version: 2020_08_05_173101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_153448) do
     t.integer "beer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rating"
   end
 
   create_table "users", force: :cascade do |t|
