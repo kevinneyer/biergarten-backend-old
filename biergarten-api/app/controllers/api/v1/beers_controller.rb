82cb7a3f647833e8c612002a8923e44f2f030a42
kevinneyer <kevinneyer@gmail.com>
2020-08-06 14:09:49 -0400

    updated seeds and controllers

--- biergarten-api/app/controllers/api/v1/beers_controller.rb
+++ biergarten-api/app/controllers/api/v1/beers_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::BeersController < ApplicationController
 
     def index
       beers = Beer.all 
-      render json: beers #include favorites and reviews
+      render json: beers, include: [:favorites, :reviews]
     end
 
     def show
         beer = Beer.find(params[:id])
-        render json: beer #include favorites and reviews
+        render json: beer, include: [:favorites, :reviews]
     end
     
 end
--- biergarten-api/app/controllers/api/v1/users_controller.rb
+++ biergarten-api/app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::UsersController < ApplicationController
 
     def index
       users = User.all 
-      render json: users #include favorites and reviews
+      render json: users, include: [:favorites, :reviews]
     end
 
     def show
       user = User.find(params[:id])
-      render json: user #include favorites and reviews
+      render json: user, include: [:favorites, :reviews]
     end
 
     
