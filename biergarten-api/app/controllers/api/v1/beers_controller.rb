0c2422d53bf0d6f1abfa2b8f500fd8ee723f0474
kevinneyer <kevinneyer@gmail.com>
2020-08-06 17:52:20 -0400

    update controller action in beers

--- biergarten-api/app/controllers/api/v1/beers_controller.rb
+++ biergarten-api/app/controllers/api/v1/beers_controller.rb
@@ -9,5 +9,17 @@ class Api::V1::BeersController < ApplicationController
         beer = Beer.find(params[:id])
         render json: beer, include: [:favorites, :reviews]
     end
+
+    def update
+      beer = Beer.find(params[:id])
+      beer.update(beer_params)
+      render json: beer, include: [:favorites, :reviews]
+    end
     
+    private
+
+    def beer_params
+      params.require(:beer).permit!
+    end
+
 end
