739bb8102a023027c3755208fe1522adc77a4aab
kevinneyer <kevinneyer@gmail.com>
2020-08-08 14:10:43 -0400

    added bcrypt and password digest

--- biergarten-api/app/models/user.rb
+++ biergarten-api/app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :beers, through: :reviews 
     has_many :favorites
     has_many :beers, through: :favorites
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true
 end
