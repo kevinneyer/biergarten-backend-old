0c359ebd39a42e76f708aeb355c320a8ddc1edd9
kevinneyer <kevinneyer@gmail.com>
2020-08-09 13:56:48 -0400

    commit before trying serializers

--- biergarten-api/app/models/user.rb
+++ biergarten-api/app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
+  
     has_many :reviews
     has_many :beers, through: :reviews 
     has_many :favorites
     has_many :beers, through: :favorites
 
-    validates :username, presence: true
+   # validates :username, presence: true
     validates :username, uniqueness: true
-    validates :password, presence: true
-
+   # validates :password, presence: true
+    #validates :password, confirmation: true
     has_secure_password
+  
 
 end
