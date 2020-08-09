2be65bf578a901264784df74641b8cc9a53f2d14
kevinneyer <kevinneyer@gmail.com>
2020-08-09 16:24:40 -0400

    more serializer logic

--- biergarten-api/app/serializers/review_serializer.rb
+++ biergarten-api/app/serializers/review_serializer.rb
@@ -1,3 +1,14 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :content, :beer_id, :user_id
+  attributes :id, :content, :beer, :user
+
+  def person
+    {user_id: self.object.user.id, 
+     user_name: self.object.user.username}
+  end 
+
+  def drink
+    {beer_id: self.object.beer.id, 
+      beer_name: self.object.beer.name}
+  end
+  
 end
